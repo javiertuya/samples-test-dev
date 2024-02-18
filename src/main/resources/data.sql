d3fd5aafda74d4db1a76219facc4f05b07832b39
AlvaroPradoRodriguez <uo284127@uniovi.es>
2024-02-18 15:20:10 +0100

    Database initial schema and data insertions to start developing

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,26 @@
 --Datos para carga inicial de la base de datos
 
 --Para giis.demo.tkrun:
-delete from carreras;
-insert into carreras(id,inicio,fin,fecha,descr) values 
-	(100,'2016-10-05','2016-10-25','2016-11-09','finalizada'),
-	(101,'2016-10-05','2016-10-25','2016-11-10','en fase 3'),
-	(102,'2016-11-05','2016-11-09','2016-11-20','en fase 2'),
-	(103,'2016-11-10','2016-11-15','2016-11-21','en fase 1'),
-	(104,'2016-11-11','2016-11-15','2016-11-22','antes inscripcion');
+delete from Course;
+delete from Registration;
+delete from Invoice;
+delete from Payment;
+
+-- Datos para la tabla Course
+insert into Course values
+(1, 'Python Programming Course', 'Learn basic Python', 1, '2024-03-01', '2024-02-01', '2024-02-15', 30, 500, 'Active', 'Juan Pérez'),
+(2, 'Graphic Design course', 'Develop basic Photoshop and ilustration skills', 2, '2024-04-25', '2024-02-01', '2024-03-15', 25, 600, 'Active', 'Ana Gómez');
+(3, 'Software Engineering', 'Learn how to create domain models', 2, '2024-05-10', '2024-03-01', '2024-04-15', 25, 600, 'Active', 'José Fanjul');
+
+
+-- Datos para la tabla Registration
+insert into Registration values
+(1, 'Carlos', 'González', '123456789', 'carlos@email.com', '2024-02-01', 'Confirmed', 1),
+(2, 'María', 'López', '987654321', 'maria@email.com', '2024-02-05', 'Received', 2);
+(3, 'Nombre', 'Random', '666777888', 'random@yopmail.com', '2024-02-12', 'Received', 2);
+
+-- Datos para la tabla Payment
+insert into Payment values
+(1, 500, '2024-02-15', 1);
+(2, 300, '2024-02-18', 3);
+
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,6 +2,39 @@
 --(en este caso en cada aplicacion se usa solo una tabla, por lo que no hace falta)
 
 --Para giis.demo.tkrun:
-drop table Carreras;
-create table Carreras (id int primary key not null, inicio date not null, fin date not null, fecha date not null, descr varchar(32), check(inicio<=fin), check(fin<fecha));
+drop table Course;
+drop table Registration;
+drop table Payment;
+drop table Invoice;
 
+create table Course 
+(course_id int primary key not null, 
+course_name varchar(50) not null, 
+objectives varchar(200) not null, 
+course_location varchar(50) not null, 
+course_date date not null, 
+course_start_regperiod date not null, 
+course_end_regperiod date not null, 
+total_places int not null, 
+course_fee int not null,
+course_state varchar(50) not null, 
+teacher_name varchar (50) not null,
+check(course_start_period <= course_end_period), 
+check (course_end_period < course_start_date));
+
+create table Registration 
+(reg_id int primary key not null, 
+reg_name varchar(50) not null, 
+reg_surnames varchar(50) not null, 
+reg_phone varchar(9) not null, 
+reg_email varchar(100) not null, 
+reg_date date not null, 
+reg_state varchar(50) not null, 
+course_id int, 
+foreign key(course_id) references Course(course_id));
+
+create table Payment 
+(payment_id int primary key not null, amount int not null, 
+payment_date date not null,
+reg_id int, 
+foreign key (reg_id) references Registration(reg_id));
